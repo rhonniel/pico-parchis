6e90cfe7065bd3bfebc919eff885c1150db456f9
larizza <larizza@mctekk.com>
2021-05-20 07:14:48 -0400

    feat: add game file to manage game

---
+++ game.lua
@@ -0,0 +1,13 @@
+-- Manages the game, the same as parchis but
+-- in a different file to facilitate version control
+
+function _init()
+end
+
+function _update()
+end
+
+function _draw()
+  cls()
+  print("pico-parchis")
+end
